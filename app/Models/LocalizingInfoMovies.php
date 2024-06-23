b06acc35979e65a3ba4d4c6b342a1420ee9c4d19
Dmitriy.D <f.doc2@yandex.ru>
2024-06-23 15:44:33 +0300

    Create model LocalizingInfoMovies

--- app/Models/LocalizingInfoMovies.php
+++ app/Models/LocalizingInfoMovies.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AssignPoster extends Model
+class LocalizingInfoMovies extends Model
 {
     use HasFactory;
     protected $guarded = false;
-    protected $table = 'assign_posters';
+    protected $table = 'localizing_info_movies';
     public $timestamps = false;
 }
