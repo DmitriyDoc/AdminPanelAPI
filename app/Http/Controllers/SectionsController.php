ea904dbc80441c1570b657632f8b89d8e552b435
Dmitriy.D <f.doc2@yandex.ru>
2024-10-18 21:28:46 +0300

    Section Controller: title Fix

--- app/Http/Controllers/SectionsController.php
+++ app/Http/Controllers/SectionsController.php
@@ -22,6 +22,7 @@ class SectionsController extends Controller
             5=>'black',
             6=>'brown',
             7=>'red',
+            8=>'silver',
         ];
 
         if (in_array($slug,$allowedSectionsNames)){
@@ -36,87 +37,86 @@ class SectionsController extends Controller
             $collection = collect();
             $collectionResponse = [];
 
-            array_walk($moviesIds, function($item, $key) use (&$TypeFilmArray) {
-                $TypeFilmArray[$item['type_film']][] = $item['id_movie'];
-            });
-
-            foreach ($TypeFilmArray as $key => $item){
-                $model = convertVariableToModelName('IdType',$key, ['App', 'Models']);
-                $allowedFilterFields = $model->getFillable();
-                if ($request->has('search')){
-                    $searchQuery = trim(strtolower(strip_tags($request->query('search'))));
-                    $model = $model->whereIn('id_movie',$item)->where($allowedFilterFields[2],'like','%'.$searchQuery.'%')->orWhere($allowedFilterFields[1],'like','%'.$searchQuery.'%');
-                }
-                $collection->add($model->select('type_film','id_movie','title','year','created_at','updated_at')->whereIn('id_movie',$item)->with(['assignPoster','categories'])->get()->all());
-            }
-            $collapsed = $collection->collapse();
-            $sorted = $collapsed->sort();
-
-            if ($sorted[0]){
-                $allowedSortFields = ['desc','asc'];
-
-                $limit = $request->query('limit',50);
-                $sortDir = strtolower($request->query('spin','asc'));
-                $sortBy = $request->query('orderBy','updated_at');
-                $perPage = $request->query('page',1);
-                if (!in_array($sortBy,$allowedFilterFields)){
-                    $sortBy = $allowedSortFields[0];
-                }
-                $collectionSort = $sorted->sortBy($sortBy)->forPage($perPage,$limit);
-                if (in_array($sortDir,$allowedSortFields)){
-                    if ($sortDir == 'asc'){
-                        $collectionSort = $collectionSort->sortByDesc($sortBy);
+            if (!empty($moviesIds)){
+                array_walk($moviesIds, function($item, $key) use (&$TypeFilmArray) {
+                    $TypeFilmArray[$item['type_film']][] = $item['id_movie'];
+                });
+                foreach ($TypeFilmArray as $key => $item){
+                    $model = convertVariableToModelName('IdType',$key, ['App', 'Models']);
+                    $allowedFilterFields = $model->getFillable();
+                    if ($request->has('search')){
+                        $searchQuery = trim(strtolower(strip_tags($request->query('search'))));
+                        $model = $model->whereIn('id_movie',$item)->where($allowedFilterFields[2],'like','%'.$searchQuery.'%')->orWhere($allowedFilterFields[1],'like','%'.$searchQuery.'%');
                     }
+                    $collection->add($model->select('type_film','id_movie','title','year','created_at','updated_at')->whereIn('id_movie',$item)->with(['assignPoster','categories'])->get()->all());
                 }
-                $collectionSortArr = $collectionSort->values()->toArray();
+                $collapsed = $collection->collapse();
+                $sorted = $collapsed->sort();
+                if ($sorted[0]){
+                    $allowedSortFields = ['desc','asc'];
 
-                foreach ($collectionSortArr as $movieItem){
-                    if ($movieItem['assign_poster']){
-                        $idsPostersArr[$movieItem['assign_poster']['type_film']][] = $movieItem['assign_poster']['id_poster_original'];
+                    $limit = $request->query('limit',50);
+                    $sortDir = strtolower($request->query('spin','asc'));
+                    $sortBy = $request->query('orderBy','updated_at');
+                    $perPage = $request->query('page',1);
+                    if (!in_array($sortBy,$allowedFilterFields)){
+                        $sortBy = $allowedSortFields[0];
                     }
-                }
-                if (!empty($idsPostersArr)){
-                    $posterCollection = collect();
-                    foreach ($idsPostersArr as $key => $item){
-                        $model = convertVariableToModelName('Posters',$key, ['App', 'Models']);
-                        $posterCollection->add($model->select('srcset','id_movie')->whereIn('id',$item)->get()->all());
+                    $collectionSort = $sorted->sortBy($sortBy)->forPage($perPage,$limit);
+                    if (in_array($sortDir,$allowedSortFields)){
+                        if ($sortDir == 'asc'){
+                            $collectionSort = $collectionSort->sortByDesc($sortBy);
+                        }
                     }
-                    $collapsedPosters = $posterCollection->collapse()->toArray();
-                }
-                foreach ($collectionSort->values()->toArray() as $k => $item) {
-                    if (!empty($section[0])){
-                        foreach ($section[0]['children'] as $col){
-                            foreach ($item['categories'] as $key => $cat){
-                                if (!empty($cat['collection_id'] )){
-                                    if ($cat['collection_id'] == $col['id'] ){
-                                        $collectionResponse['data'][$k]['collection'][$key]['label'] = $col['label'];
-                                        $collectionResponse['data'][$k]['collection'][$key]['value'] = $col['value'];
+                    $collectionSortArr = $collectionSort->values()->toArray();
+
+                    foreach ($collectionSortArr as $movieItem){
+                        if ($movieItem['assign_poster']){
+                            $idsPostersArr[$movieItem['assign_poster']['type_film']][] = $movieItem['assign_poster']['id_poster_original'];
+                        }
+                    }
+                    if (!empty($idsPostersArr)){
+                        $posterCollection = collect();
+                        foreach ($idsPostersArr as $key => $item){
+                            $model = convertVariableToModelName('Posters',$key, ['App', 'Models']);
+                            $posterCollection->add($model->select('srcset','id_movie')->whereIn('id',$item)->get()->all());
+                        }
+                        $collapsedPosters = $posterCollection->collapse()->toArray();
+                    }
+                    foreach ($collectionSort->values()->toArray() as $k => $item) {
+                        if (!empty($section[0])){
+                            foreach ($section[0]['children'] as $col){
+                                foreach ($item['categories'] as $key => $cat){
+                                    if (!empty($cat['collection_id'] )){
+                                        if ($cat['collection_id'] == $col['id'] ){
+                                            $collectionResponse['data'][$k]['collection'][$key]['label'] = $col['label'];
+                                            $collectionResponse['data'][$k]['collection'][$key]['value'] = $col['value'];
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                    if (!empty($collapsedPosters)){
-                        foreach ($collapsedPosters as $posterItem){
-                            if ($item['id_movie'] == $posterItem['id_movie']){
-                                $img = explode(',',$posterItem['srcset'] ?? '');
-                                $collectionResponse['data'][$k]['poster'] = $img[0] ?? '';
+                        if (!empty($collapsedPosters)){
+                            foreach ($collapsedPosters as $posterItem){
+                                if ($item['id_movie'] == $posterItem['id_movie']){
+                                    $img = explode(',',$posterItem['srcset'] ?? '');
+                                    $collectionResponse['data'][$k]['poster'] = $img[0] ?? '';
+                                }
                             }
                         }
+                        $collectionResponse['data'][$k]['created_at'] = date('Y-m-d', strtotime($item['created_at'])) ?? '';
+                        $collectionResponse['data'][$k]['updated_at'] = date('Y-m-d', strtotime($item['updated_at'])) ?? '';
+                        $collectionResponse['data'][$k]['title'] = $item['title'] ?? '';
+                        $collectionResponse['data'][$k]['year'] = $item['year'] ?? null;
+                        $collectionResponse['data'][$k]['id_movie'] = $item['id_movie'] ?? '';
+                        $collectionResponse['data'][$k]['type_film'] = $item['type_film'] ?? '';
                     }
-                    $collectionResponse['data'][$k]['created_at'] = date('Y-m-d', strtotime($item['created_at'])) ?? '';
-                    $collectionResponse['data'][$k]['updated_at'] = date('Y-m-d', strtotime($item['updated_at'])) ?? '';
-                    $collectionResponse['data'][$k]['title'] = $item['title'] ?? '';
-                    $collectionResponse['data'][$k]['year'] = $item['year'] ?? null;
-                    $collectionResponse['data'][$k]['id_movie'] = $item['id_movie'] ?? '';
-                    $collectionResponse['data'][$k]['type_film'] = $item['type_film'] ?? '';
+                    $collectionResponse['collections'] = $section[0]['children'];
+                    $collectionResponse['total'] = $collapsed->count();
                 }
-                $collectionResponse['title'] = $section[0]['title'];
-                $collectionResponse['total'] = $collapsed->count();
-                $collectionResponse['collections'] = $section[0]['children'];
-                return $collectionResponse;
             }
-
+            $collectionResponse['title'] = $section[0]['title'];
+            return $collectionResponse;
         }
         return [];
     }
