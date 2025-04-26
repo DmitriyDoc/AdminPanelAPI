2e8b2835714d32d8231b4937d54b6d887e9087cb
Dmitriy.D <f.doc2@yandex.ru>
2025-04-26 21:19:25 +0300

    Add method parseHttpHeaders in helpers.php.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -177,4 +177,23 @@ if (!function_exists('cascaderStructure')) {
             }
         }
     }
+    if (!function_exists('parseHttpHeaders')) {
+        function parseHttpHeaders( $headers )
+        {
+            $head = [];
+            foreach( $headers as $k => $v )
+            {
+                $t = explode( ':', $v, 2 );
+                if( isset( $t[1] ) )
+                    $head[ trim($t[0]) ] = trim( $t[1] );
+                else
+                {
+                    $head[] = $v;
+                    if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                        $head['response_code'] = intval($out[1]);
+                }
+            }
+            return $head;
+        }
+    }
 }
