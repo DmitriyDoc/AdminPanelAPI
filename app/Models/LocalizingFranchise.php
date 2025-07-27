7c355ff99f9e8fa8414fa56660ca997b0a974e55
Dmitriy.D <f.doc2@yandex.ru>
2025-07-27 22:51:29 +0300

    Change table name

--- app/Models/LocalizingFranchise.php
+++ app/Models/LocalizingFranchise.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class LocalizingFranchise extends Model
 {
     use HasFactory;
-    protected $table = 'localizing_franchise';
+    protected $table = 'franchises';
     public $timestamps = false;
 
     protected $fillable = [
